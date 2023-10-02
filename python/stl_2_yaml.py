b18bece7dec4f466421ebc4785f7ec0922f2bd4d
Uri Ziv <uriziv12@gmailcom>
2023-10-02 13:38:16 +0300

    Update stl_2_yaml

--- python/stl_2_yaml.py
+++ python/stl_2_yaml.py
@@ -1,5 +1,9 @@
 
+import os
 
-PROJECT_ROOT_DIR = ""
-STL_ROOT_DIR = ""
-YAML_ROOT_DIR = ""
+SCR_DIR = os.path.dirname(os.path.realpath(__file__))
+PROJECT_ROOT_DIR = os.path.dirname(SCR_DIR)
+STL_ROOT_DIR = os.path.join(PROJECT_ROOT_DIR, "stl")
+YAML_ROOT_DIR = os.path.join(PROJECT_ROOT_DIR, "yaml")
+
+
