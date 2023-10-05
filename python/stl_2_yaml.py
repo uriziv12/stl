2e5c19aab52fe40a47af0b718c0eea4fcb9de994
Uri Ziv <uriziv12@gmailcom>
2023-10-05 20:14:30 +0300

    Update stl_2_yaml.py ==> bk_67_2_1_text.yaml

--- python/stl_2_yaml.py
+++ python/stl_2_yaml.py
@@ -21,19 +21,25 @@ stl_filepath = os.path.join(STL_ROOT_DIR, filename + STL_FILE_EXTENSION)
 yaml_filepath = os.path.join(YAML_ROOT_DIR, filename + YAML_FILE_EXTENSION)
 
 # Functions
-def string_2_yaml_line(string_i):
+def string_2_yaml_line(string_i, prefix_for_text=None):
     regex_pttr = "(^\s*)(.*$)"
     regex_result = re.search(regex_pttr, decoded_line_i)
     leading_spaces_i = regex_result.group(1)
     rest_of_line_i = regex_result.group(2)
-    return "{}\"{}\":\n".format(leading_spaces_i, rest_of_line_i)
+    ret_val = "{}\"{}\":\n".format(leading_spaces_i, rest_of_line_i)
+    if prefix_for_text is not None:
+        ret_val = "{}\"{}{}\":\n".format(leading_spaces_i, prefix_for_text, rest_of_line_i)
+    return ret_val
 
 
 # Write yaml file based on stl file
 yaml_f = open(yaml_filepath, "w+")
 with io.open(stl_filepath, 'rb') as stl_f:
-    for line_i in stl_f:
+    for line_num_i, line_i in enumerate(stl_f):
         decoded_line_i = line_i.decode("utf-8")
-        yaml_line_i = string_2_yaml_line(decoded_line_i)
+        line_num_formatted_i = '{:>04d}> '.format(line_num_i)
+        yaml_line_i = string_2_yaml_line(decoded_line_i, prefix_for_text=line_num_formatted_i)
         yaml_f.write(yaml_line_i)
 yaml_f.close()
+
+print("File {} written.".format(yaml_filepath))
